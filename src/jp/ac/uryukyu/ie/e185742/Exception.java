6af3a8a654d08ab24a5ac91d5d6ae2aa80d50c55
Yuto Matayoshi <e185742@ie.u-ryukyu.ac.jp>
2018-11-26 00:45:54 +0900

    Step3 first commit

--- src/jp/ac/uryukyu/ie/e185742/Exception.java
+++ src/jp/ac/uryukyu/ie/e185742/Exception.java
@@ -2,13 +2,8 @@ package jp.ac.uryukyu.ie.e185742;
 
 public class Exception {
     public static void main(String[] args){
-        try{
-            String str = null;
-            System.out.println(str.length());
-
-        }catch (NullPointerException e){
-            System.out.println("ぬるぽ！");
-            System.out.println(e.getMessage());
-        }
+        String str = "百二十三";
+        int value;
+        value = Integer.parseInt(str);
     }
 }
