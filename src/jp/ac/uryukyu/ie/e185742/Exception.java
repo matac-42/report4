309ae0f9442875bbe6f6fbdb9fcd53eee5e47311
Yuto Matayoshi <e185742@ie.u-ryukyu.ac.jp>
2018-11-26 00:37:31 +0900

    Add try-catch

--- src/jp/ac/uryukyu/ie/e185742/Exception.java
+++ src/jp/ac/uryukyu/ie/e185742/Exception.java
@@ -2,7 +2,13 @@ package jp.ac.uryukyu.ie.e185742;
 
 public class Exception {
     public static void main(String[] args){
-        String str = null;
-        System.out.println(str.length());
+        try{
+            String str = null;
+            System.out.println(str.length());
+
+        }catch (NullPointerException e){
+            System.out.println("ぬるぽ！");
+            System.out.println(e.getMessage());
+        }
     }
 }
